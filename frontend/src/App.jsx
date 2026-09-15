7445e9b397a6d3b51042d499de6b70eec21618c2
Vanessa <vanessa@mac.home>
2026-09-15 18:34:43 -0400

    Configure frontend for GitHub Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import MoleculeStructure from "./MoleculeStructure";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://127.0.0.1:8000";
 
 function App() {
   // ==================================================
